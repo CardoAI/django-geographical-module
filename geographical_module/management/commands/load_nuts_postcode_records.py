e496154e670acfb3db9a5487bfe6d406de188e52
Kristi Kotini <kristi.kotini@cardoai.com>
2022-01-12 11:17:03 +0100

    added command to load nuts3-postcode data in db

---
+++ geographical_module/management/commands/load_nuts_postcode_records.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from scripts.nuts_postcode import create_nuts_postcode_records_for_db
+
+
+class Command(BaseCommand):
+    help = 'Load nuts3-postcode data from csv file into the database'
+
+    def handle(self, **options):
+        create_nuts_postcode_records_for_db()
